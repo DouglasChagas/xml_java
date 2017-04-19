3e43c9cb877db113b6751d36250337e97431d546
douglas.chagas <douglas.chagas@DC0001>
2017-04-19 17:32:26 -0300

    2

--- src/br/com/dotchagas/xml_java/Sistema.java
+++ src/br/com/dotchagas/xml_java/Sistema.java
@@ -8,7 +8,10 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+
+import br.com.dotchagas.xml_java.model.Produto;
 
 public class Sistema {
 
@@ -16,16 +19,30 @@ public class Sistema {
 
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = factory.newDocumentBuilder();
-
 		Document document = builder.parse("src/venda.xml");
 
 		String moeda = document.getDocumentElement().getAttribute("moeda");
 		System.out.println(moeda);
-		
-		Element tagFormaDePagamento = (Element) document.getElementsByTagName("formaDePagamento").item(0);
-		String formaDePagamento = tagFormaDePagamento.getTextContent();
 
-		System.out.println(formaDePagamento);
+		NodeList produtos = document.getElementsByTagName("produto");
+
+		for(int i =0; i < produtos.getLength(); i++) {
+		    Element produto = (Element) produtos.item(i);
+		    String nome = produto.getElementsByTagName("nome").item(0).getTextContent();
+		    String preco = produto.getElementsByTagName("preco").item(0).getTextContent();
+		    
+		    Produto prod = new Produto(nome,Double.parseDouble(preco));
+
+		    /*
+		    System.out.println("-----------");
+		    System.out.println("Nome do produto: "+ nome);
+		    System.out.println("Preco do produto: "+ preco);
+		    System.out.println("-----------");
+		    */
+		    System.out.println(prod);
+		}
+
+		
 	}
 
 }
